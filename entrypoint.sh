03e98854aae4c88351ac10f1d683b38b569883da
wittyphantom333 <adam@wittsgarage.com>
2023-08-31 03:38:35 +0000

    updated entrypoint logic

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,4 +19,4 @@ git clone https://github.com/VivusAI/rp-docker.git \
 set -euo pipefail
 
 # exec the final command:
-exec python server.py --listen --xformers --api --gpu 20 20
+exec python server.py --listen --xformers --api --gpu-memory 20 20
